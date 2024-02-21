0208716151b5c30724b0f8f53a586e51bfb770dd
AJ Harris <shogunaut90@gmail.com>
2024-02-20 18:28:42 -0600

    Full CRUD REST api is created with a database using MongoDB and Express

--- routes/ideas.js
+++ routes/ideas.js
@@ -1,6 +1,7 @@
 // To create seperate file you need to use a router
 const express = require('express');
 const router = express.Router();
+const Idea = require('../models/Idea');
 
 // also changed all instances of app.method() to router.method()
 
@@ -14,110 +15,75 @@ const router = express.Router();
 // We now have a working CRUD (Create, Read, Update, Delete) Api, or a full RESTFUL Api
 // What's next is using a database to persist the data, because right now this all works in memory
 
-const ideas = [
-  {
-    id: 1,
-    text: 'Postive Newsletter, a newsletter that only shares positive uplifting news',
-    tag: 'Technology',
-    username: 'CommanderSheperd',
-    date: '2024-12-01',
-  },
-  {
-    id: 2,
-    text: 'Milk cartons that turn a different color the older your milk is getting',
-    tag: 'Inventions',
-    username: 'TaliNarAya',
-    date: '2023-04-02',
-  },
-  {
-    id: 3,
-    text: 'ATM location app which lets you know where the closest ATM is and if it can be calibrated',
-    tag: 'Software',
-    username: 'JustGarrus',
-    date: '2023-15-12',
-  }
-]
-
 // Get all ideas
-router.get('/', (req, res) => {
-  // You could just send the response this way
-  // res.json(ideas);
-  // But this is a nicer more dev friendly way of doing it
-  res.json({ success: true, data: ideas});
+router.get('/', async (req, res) => {
+  try {
+    const ideas = await Idea.find();
+    res.json({ success: true, data: ideas});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'Something went wrong'});
+  }
 });
 
 // get a single idea
-// the :id is a queryparam accessed by the req object
-router.get('/:id', (req, res) => {
-  // access :id
-  req.params.id;
-  // When finding by id it's easier in Databases but in this case we need to manually do it with high order array methods like .find()
-  const idea = ideas.find((idea) => idea.id === +req.params.id); // the + parsed the id from a string to a number
-  // ERROR handing (say there is no idea with the requested id)
-  if (!idea) {
-    return res.status(404).json( {success: false, error: 'Resource not found'});
+router.get('/:id', async (req, res) => {
+
+  try {
+    const idea = await Idea.findById(req.params.id);
+    res.json({ success: true, data: idea});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'Something went wrong'});
   }
-  res.json({ success: true, data: idea});
 });
 
 // Add an idea by accepting post requests
-router.post('/', (req, res) => {
-  // With the data sent, we want to accept text, tag, and username
-  // We are dealing with static data so we create the id ourselves, but a database creates it automatically
-  const idea = {
-    id: ideas.length + 1,
+router.post('/', async (req, res) => {
+  const idea = new Idea ({
     text: req.body.text,
     tag: req.body.tag,
-    username: req.body.username,// Normally user authentication would be used but that is beyond the scope of this project
-    data: new Date().toISOString().slice(0, 10)
+    username: req.body.username
+  });
+
+  try {
+    const savedIdea = await idea.save();
+    res.json({ sucess: true, data: savedIdea });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'Something went wrong'});
   }
-  // console.log(idea);
-  ideas.push(idea);
-  res.json({ sucess: true, data: idea });
 })
 
 // Update idea
-router.put('/:id', (req, res) => {
-  req.params.id;
-  const idea = ideas.find((idea) => idea.id === +req.params.id);
-  if (!idea) {
-    return res.status(404).json( {success: false, error: 'Resource not found'});
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedIdea = await Idea.findByIdAndUpdate(
+      req.params.id, 
+      {
+        $set: {
+          text: req.body.text,
+          tag: req.body.tag
+        }
+      },
+      { new: true }
+    );
+    res.json({sucess: true, data: updatedIdea});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'Something went wrong'});
   }
-
-  idea.text = req.body.text || idea.text;
-  idea.tag = req.body.tag || idea.tag;
-
-  res.json({ success: true, data: idea });
-
 });
 
 // Delete an idea
-router.delete('/:id', (req, res) => {
-  req.params.id;
-  const idea = ideas.find((idea) => idea.id === +req.params.id);
-  if (!idea) {
-    return res.status(404).json( {success: false, error: 'Resource not found'});
+router.delete('/:id', async (req, res) => {
+  try {
+    await Idea.findByIdAndDelete(req.params.id);
+    res.json({ success: true, data: {} });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'Something went wrong'});
   }
-
-  /*
-    Brad wanted us to try on our own first
-
-    This was my solution (It 100% works)
-
-    ideas.forEach((idea) => {
-      if(idea.id === +req.params.id) {
-        let index = ideas.indexOf(idea);
-        ideas.splice(index, 1);
-      }
-    })
-  */
-
-  // This was Brads solution
-  const index = ideas.indexOf(idea);
-  ideas.splice(index, 1);
-
-  res.json({ success: true, data: {} });
-
 });
 
 // don't forget to export the router
